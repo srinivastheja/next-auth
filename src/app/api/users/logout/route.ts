05c71d54967cc7fd6c9f7dfdda9fb57c65323c7b
srinivastheja <srinivasteja.vallampati@gmail.com>
2023-09-15 01:41:28 +0530

    push

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,14 +1,21 @@
 import { NextResponse } from "next/server";
 
+
 export async function GET() {
-  try {
-    const response = NextResponse.json({
-      message: "logout success",
-      success: true,
-    });
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
-    return response;
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
+    try {
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
+        });
+        return response;
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+        
+    }
+
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
-import React, { useState } from "react";
-import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
+import React, {useState} from "react";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
 
 
 export default function ProfilePage() {
@@ -14,7 +14,7 @@ export default function ProfilePage() {
             await axios.get('/api/users/logout')
             toast.success('Logout successful')
             router.push('/login')
-        } catch (error: any) {
+        } catch (error:any) {
             console.log(error.message);
             toast.error(error.message)
         }
@@ -33,18 +33,18 @@ export default function ProfilePage() {
             <p>Profile page</p>
             <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
             </Link>}</h2>
-            <hr />
-            <button
-                onClick={logout}
-                className="bg-blue-500 mt-4 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded"
-            >Logout</button>
+        <hr />
+        <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Logout</button>
 
-            <button
-                onClick={getUserDetails}
-                className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >GetUser Details</button>
+        <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
 
 
-        </div>
+            </div>
     )
 }
